5da88442808a623554ab2bdd1ce0466d56788eff
MichaelVDeSanti <59933808+MichaelVDeSanti@users.noreply.github.com>
2022-05-18 17:04:02 -0400

    Update EO_run.py

--- EO_run.py
+++ EO_run.py
@@ -1,13 +1,11 @@
 import EO_ens_SWOT
 import os
-
 from EO_ens_SWOT import EO_Ensemble
 
-data_path = "C:\\Users\\micha\\Research\\SWOT\\Data\\Operational_Data\\Test_Sets\\All_test"
-file = 'Test5.csv'
-inputtime = 9
-scenario='Minimum Decay' #Has to be 'Optimum Decay', 'Maximum Decay', or 'Minimum Decay'
+data_path = sys_arg1
+file = sys_arg2
+inputtime = sys_arg3
+scenario=sys_arg4  #Has to be 'Optimum Decay', 'Maximum Decay', or 'Minimum Decay'
 
-eo_ens = EO_Ensemble(inputtime, "C:\\Users\\micha\\Research\\SWOT\\Data\\Operational_Data\\Test_Sets\\All_test",
+eo_ens = EO_Ensemble(inputtime,data_path,
                      file, scenario)
-best_params=eo_ens.run_EO()
